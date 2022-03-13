d534cbe53caee5d2f7039a7565969a6559f9446b
Andrew <60839662+acbgbca@users.noreply.github.com>
2022-03-13 15:55:07 +1100

    Modified Plex backup to exclude unnecessary files

--- backup.sh
+++ backup.sh
@@ -14,10 +14,20 @@ echo "Backup Directory: $bd"
 
 cd $wd
 
+# Before we start, clean up the plex cache
+# This removes all cache files older than 5 days
+find "./plex/config/Library/Application Support/Plex Media Server/Cache/PhotoTranscoder" -type f -mtime +5 -delete
+
 for d in */ ; do
 	# Remove trailing slash from directory
 	d=${d%/}
 	echo "Backing up $d"
-	tar -czf $bd/$d.tgz $d
+	if [ $d = "plex" ]
+	then
+		# Exclude paths not required for rsetore
+		tar --exclude='config/Library/Application Support/Plex Media Server/Cache' --exclude='/config/Library/Application Support/Plex Media Server/Crash Reports' --exclude='config/Library/Application Support/Plex Media Server/Logs' --exclude='config/Library/Application Support/Plex Media Server/Media' -czf $bd/$d.tgz $d
+	else
+		tar -czf $bd/$d.tgz $d
+	fi
 done
 
