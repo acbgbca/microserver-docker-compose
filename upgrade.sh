57c2008d39f4392fbe2bc43f0143f89cd097943c
Andrew <60839662+acbgbca@users.noreply.github.com>
2022-06-06 20:40:16 +1000

    Fixed upgrade to rebuild Azure agent

--- upgrade.sh
+++ upgrade.sh
@@ -5,11 +5,18 @@ wd=${1:-/ctr_cfg}
 
 cd $wd
 
+export COMPOSE_DOCKER_CLI_BUILD=1
+export DOCKER_BUILDKIT=1
+
 for d in */ ; do
 	echo "Upgrading $d"
 	cd $d
+
 	# Download updated containers
 	docker-compose pull --ignore-pull-failures
+
+	# Rebuild any docker container defined in the docker-compose file
+	docker-compose build --no-cache
 
 	# Upgrade container if required
 	docker-compose up -d --remove-orphans
