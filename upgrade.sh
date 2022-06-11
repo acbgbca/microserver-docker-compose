e757a1b45216ebc4a3c49dc16dc4bb37c6120f2a
Andrew <60839662+acbgbca@users.noreply.github.com>
2022-06-11 19:44:13 +1000

    Setup Cloudflare tunnel with separate reverse proxy

--- upgrade.sh
+++ upgrade.sh
@@ -16,7 +16,7 @@ for d in */ ; do
 	docker-compose pull --ignore-pull-failures
 
 	# Rebuild any docker container defined in the docker-compose file
-	docker-compose build --no-cache
+	# docker-compose build --no-cache
 
 	# Upgrade container if required
 	docker-compose up -d --remove-orphans
